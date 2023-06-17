324af4b759cd41fe45e39b0a3e2222b457e2818b
R0rt1z2 <me@r0rt1z2.com>
2023-06-17 13:56:54 +0000

    next: Define architecture flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,5 +6,17 @@
 
 DEVICE_PATH := device/huawei/next
 
+# Arch
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
 # Inherit the proprietary files
 include vendor/huawei/next/BoardConfigVendor.mk
