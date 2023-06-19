0ec23fe253baa0b76284e9650755d7515b731629
Ethan Chen <intervigil@gmail.com>
2023-06-19 21:13:09 +0000

    next: Add shim for libbase LogMessage functions

--- device.mk
+++ device.mk
@@ -203,6 +203,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0.vendor
 
+# Shims
+PRODUCT_PACKAGES += \
+    libbase_shim
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
