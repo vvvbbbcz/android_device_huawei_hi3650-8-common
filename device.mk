24c45e070162e2e8942080d6ddaec8ddfc97a64c
Giovanni Ricca <giovanniricca@proton.me>
2023-07-15 20:30:27 +0000

    next: Build AOSP NFC service
    Change-Id: I2383f763578d74f281e951af6ee3c2b2c04991a6

--- device.mk
+++ device.mk
@@ -143,7 +143,12 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0.vendor
+    android.hardware.nfc@1.0.vendor \
+    android.hardware.nfc@1.2-service
+
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
+    Tag
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
