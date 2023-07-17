f1edeebc21d87863c8bbe0cd0a9fe06a180b406a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-17 17:12:23 +0000

    next: Build health HIDL impl for recovery
    * This is needed for non-A/B devices
    
    Change-Id: I8e5fccb8c56401679551cb3440760e516ca4efd1

--- device.mk
+++ device.mk
@@ -118,6 +118,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
+    android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-impl
 
 # HIDL
