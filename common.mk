0d154b187818c64888670560260fc6d81e239711
Vaisakh Murali <mvaisakh@statixos.com>
2023-09-18 15:37:31 +0000

    hi3650-8-common: Build libpcap from source
    Use newer OSS parts rather than older prebuilts.
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: If35386d80fb200ad35e855bc19abff82b1b71de0
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/6635
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- common.mk
+++ common.mk
@@ -176,6 +176,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Pcap
+PRODUCT_PACKAGES += \
+    libpcap.vendor
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
